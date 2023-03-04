58e958559de8925d3691c7dbab2ad6a1a4b6801b
Varun <31084854+varunsingh87@users.noreply.github.com>
2023-03-03 22:45:22 -0500

    Add metrics, project requests page

---
+++ index.mjs
@@ -0,0 +1,43 @@
+import * as http from 'node:http'
+import * as fs from 'node:fs'
+import * as path from 'node:path'
+
+const PORT = 3000
+
+const MIME_TYPES = {
+    default: 'application/octet-stream',
+    html: 'text/html; charset=UTF-8',
+    js: 'application/javascript',
+    css: 'text/css',
+    png: 'image/png',
+    jpg: 'image/jpg',
+    gif: 'image/gif',
+    ico: 'image/x-icon',
+    svg: 'image/svg+xml',
+}
+
+const STATIC_PATH = path.join(process.cwd(), './')
+
+const prepareFile = async (url) => {
+    const paths = [STATIC_PATH, url]
+    if (url.endsWith('/')) paths.push('index.html')
+    const filepath = path.join(...paths)
+    const pathTraversal = !filepath.startsWith(STATIC_PATH)
+    const exists = await fs.promises.access(filepath).then(() => true, () => false)
+    const found = !pathTraversal && exists;
+    const streamPath = found ? filepath : STATIC_PATH + '/404.html'
+    const ext = path.extname(streamPath).substring(1).toLowerCase()
+    const stream = fs.createReadStream(streamPath)
+    return { found, ext, stream }
+}
+
+http.createServer(async (req, res) => {
+    const file = await prepareFile(req.url)
+    const statusCode = file.found ? 200 : 404
+    const mimeType = MIME_TYPES[file.ext] || MIME_TYPES.default;
+    res.writeHead(statusCode, { 'Content-Type': mimeType })
+    file.stream.pipe(res);
+    console.log(`${req.method} ${req.url} ${statusCode}`)
+}).listen(PORT)
+
+console.log(`Server tunning at http://localhost:${PORT}`)
